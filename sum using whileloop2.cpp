3caf56984e257d15168c50a53d0b7cd69f526eb7
Sathwikak96 <97940151+Sathwikak96@users.noreply.github.com>
2022-02-21 11:07:00 +0530

    Add files via upload

---
+++ sum using whileloop2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=0, n, num, sum=0;
+    cout<<"enter the 10 numbers";
+    while(i<n)
+    {
+        cin>>n;
+        sum = sum+n;
+        i++;
+    }
+    cout<<"sum of 10 numbers are:"<<sum<<endl;
+    return 0;
+}
